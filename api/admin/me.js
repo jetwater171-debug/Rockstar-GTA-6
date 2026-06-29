a07b8825469cc7613a289364200b9c6ecafb8429
jetwater171-debug <jetwater171@gmail.com>
2026-06-28 23:58:08 -0300

    Add admin session endpoint

---
+++ api/admin/me.js
@@ -0,0 +1,7 @@
+import { ensureAllowedRequest, sendJson, verifyAdminCookie } from '../_utils.js';
+
+export default function handler(req, res) {
+  if (!ensureAllowedRequest(req, res, { requireSession: false })) return;
+  if (!verifyAdminCookie(req)) return sendJson(res, 401, { ok: false });
+  sendJson(res, 200, { ok: true });
+}
