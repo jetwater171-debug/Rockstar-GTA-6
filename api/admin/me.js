26844a9533e7912233336adb7094da12781cb555
jetwater <jetwater171@gmail.com>
2026-08-11 04:26:44 -0300

    Stay within Vercel Hobby function limit

--- api/admin/me.js
+++
@@ -1,7 +0,0 @@
-import { ensureAllowedRequest, sendJson, verifyAdminCookie } from '../../lib/api-utils.js';
-
-export default function handler(req, res) {
-  if (!ensureAllowedRequest(req, res, { requireSession: false })) return;
-  if (!verifyAdminCookie(req)) return sendJson(res, 401, { ok: false });
-  sendJson(res, 200, { ok: true });
-}
